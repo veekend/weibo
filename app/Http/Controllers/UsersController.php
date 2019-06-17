d555c4abbad181d212694b52b78c50b412c9db8c
veekend <670011958@qq.com>
2019-06-17 08:39:04 +0000

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     public function create()
     {
         return view('users.create');
+    }
+
+    public function show(User $user)
+    {
+        ##对象转化为一个关联数组
+        return view('users.show', compact('user'));
     }
 }
